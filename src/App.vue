73f0f93e7879a8d3845fb2fb0711a327e45b2e3f
diegosantosouza <diegosantos.s@hotmail.com>
2021-10-08 00:52:58 -0300

    Pokemon component
    create Pokemon component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="app">
-    
+    <div v-for="(poke,index) in pokemons" :key="poke.url">
+      <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+    </div>
   </div>
 </template>
 
 <script>
-//import axios from 'axios';
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
 export default {
   name: 'App',
+  data(){
+    return {
+      pokemons: []
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      this.pokemons = res.data.results;
+    })
+  },
+  components:{
+    Pokemon
+  },
 }
 </script>
 
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{num}} - {{name | upper}}</h1>
+        <small>{{url}}</small>
+    </div>
+</template>
+
+<script>
+export default {
+    porps: {
+        num: Number,
+        name: String,
+        url: String
+    },
+    filters: {
+        upper: function(value){
+            var newName = value[0].toUpperCase() + value.slice(1);
+            return newName;
+        }
+    }
+}
+</script>
+<style>
+
+</style>
